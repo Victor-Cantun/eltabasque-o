5e5ce03bf2524839b8ff193f871c8784a1d73031
Victor Cantun <victorcantun@icloud.com>
2026-09-14 13:49:54 -0600

    Corrección de sidebar|dashboard

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -96,7 +96,7 @@ export function AppSidebar() {
                 <SidebarMenu>
                     <SidebarMenuItem>
                         <SidebarMenuButton size="lg" asChild>
-                            <Link href={dashboard()} prefetch>
+                            <Link href={dashboardUrl} prefetch>
                                 <AppLogo />
                             </Link>
                         </SidebarMenuButton>
